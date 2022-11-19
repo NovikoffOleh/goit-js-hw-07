c3e34d41e1d21773c6dea60eba85ce806b027264
NovikoffOleh <novikoff10000@gmail.com>
2022-11-19 13:50:31 +0200

    correct

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,97 +1,58 @@
-const galleryItems = [
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-      description: 'Hokkaido Flower',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-      description: 'Container Haulage Freight',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-      description: 'Aerial Beach View',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-      description: 'Flower Blooms',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-      description: 'Alpine Mountains',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-      description: 'Mountain Lake Sailing',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-      description: 'Alpine Spring Meadows',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-      description: 'Nature Landscape',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-      description: 'Lighthouse Coast Sea',
-    },
-  ];
-  
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const imagesContainer = document.querySelector('.gallery');
 
-galleryItems.forEach(({preview,original,description }) => {
-const imageEl = document.createElement('img'); 
-   
-   imageEl.src = preview;
-   imageEl.source = original;
-   imageEl.alt = description;
-   imagesContainer.append(imageEl)
-});
-  
-  
+const galleryEl = document.querySelector(".gallery");
+const galleryItemMarkup = creatGalleryMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", galleryItemMarkup);
 
+function creatGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+         <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+             src="${preview}"
+            data-source="${original}"
+             alt="${description}"
+         />
+         </a>
+       </div>`;
+    })
+    .join("");
+}
 
+galleryEl.addEventListener("click", openImgModal);
 
+function openImgModal(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const imgUrlOriganal = event.target.dataset.source;
 
+  const instance = basicLightbox.create(
+    `
+    <div class="modal">
+       <img src="${imgUrlOriganal}" width="800" height="600">
+       <a>Close</a>
+    </div>
+`,
+    {
+      onShow: () => {
+        document.addEventListener("keyup", onEscClose);
+      },
+      onClose: () => {
+        document.removeEventListener("keyup", onEscClose);
+      },
+    }
+  );
 
+  function onEscClose(evt) {
+    if (evt.key === "Escape") {
+      instance.close();
+    }
+  }
 
-
-
-
-//import * as basicLightbox from 'basiclightbox'
-
-//const instance = basicLightbox.create(`
-//    <img src="assets/images/image.png" width="800" height="600">
-//`)
-
-//instance.show()
-
-console.log(galleryItems);
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+const galleryItemMarkup = creatGalleryMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", galleryItemMarkup);
+
+function creatGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li>
+         <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+       </li>`;
+    })
+    .join("");
+}
+const lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
